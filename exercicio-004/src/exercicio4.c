201c6cf6d7ab213c640187104199355650c23570
sombriks <sombriks@gmail.com>
2017-11-05 12:34:58 -0300

    floreios

--- exercicio-004/src/exercicio4.c
+++ exercicio-004/src/exercicio4.c
@@ -3,20 +3,28 @@
 
 void *th(void *param) {
   parameter_t *p = (parameter_t *)param;
+  printf("thread %d will use %d resources\n", p->id, p->num);
   ex_wait(p->num);
-  printf("thread %d using %d resources\n", p->id, p->num);
+  printf("consuming resource\n");
   ex_signal(p->num);
+  printf("thread %d ending\n", p->id);
   return NULL;
 }
 
 void ex_wait(int num) {
-  sem_wait(&mutex);
+  // sem_wait(&mutex);
   while (num--) sem_wait(&resource);
-  sem_post(&mutex);
+  int val;
+  sem_getvalue(&resource, &val);
+  printf("semaphore have %d resources left\n", val);
+  // sem_post(&mutex);
 }
 
 void ex_signal(int num) {
-  sem_wait(&mutex);
+  // sem_wait(&mutex);
   while (num--) sem_post(&resource);
-  sem_post(&mutex);
+  int val;
+  sem_getvalue(&resource, &val);
+  printf("semaphore have %d resources available\n", val);
+  // sem_post(&mutex);
 }
